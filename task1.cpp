214d9e24a85a7e25d483870faa8c3d9b1c382c8f
nermeen03 <nermeen41292@gmail.com>
2024-10-21 22:27:14 +0300

    Add files via upload
    Reading 5 integer numbers and finding the max and min (without looping)

---
+++ task1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main()
+{
+    int no1, no2, no3, no4, no5, Max, Min;
+    cout<<"Please enter 5 numbers (without loop): "<<endl;
+    cin>>no1;
+    cin>>no2;
+    cin>>no3;
+    cin>>no4;
+    cin>>no5;
+
+    if(no1<no2){
+        Max = no2;
+        Min = no1;
+    }
+    else{
+        Max = no1;
+        Min = no2;
+    }
+    if(Max<no3){
+        Max = no3;
+    }
+    else{
+        if(Min>no3){
+            Min = no3;
+        }
+    }
+    if(Max<no4){
+        Max = no4;
+    }
+    else{
+        if(Min>no4){
+            Min = no4;
+        }
+    }
+    if(Max<no5){
+        Max = no5;
+    }
+    else{
+        if(Min>no5){
+            Min = no5;
+        }
+    }
+    cout<<"The max is "<<Max<<endl;
+    cout<<"The min is "<<Min<<endl;
+
+    return 0;
+}
+*/
