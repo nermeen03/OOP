751aac373d75d5a6b63af91ae300112e9ff4e7c3
nermeen03 <nermeen41292@gmail.com>
2024-10-21 22:29:15 +0300

    Add files via upload
    reading 5 integer numbers and finding the min and max (with looping)

---
+++ task1(2).cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+/*
+int main()
+{
+    int no1, Max, Min;
+    cout<<"Please enter 5 numbers(with loop): "<<endl;
+    cin>>no1;
+    Max = no1;
+    Min = no1;
+    for(int i = 1; i<= 4 ; i++){
+        cin>>no1;
+        if(Max < no1){
+            Max = no1;
+        }
+        if(Min > no1){
+            Min = no1;
+        }
+    }
+    cout<<"The max is "<<Max<<endl;
+    cout<<"The min is "<<Min<<endl;
+}
+*/
