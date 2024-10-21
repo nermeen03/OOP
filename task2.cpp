d5e5e91e03fc61ff2902d6d3f0617ecd12bc659d
nermeen03 <nermeen41292@gmail.com>
2024-10-21 22:32:37 +0300

    Add files via upload
    Solving a second degree equation

---
+++ task2.cpp
@@ -0,0 +1,41 @@
+
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    double dm, x1,x2;
+    cout<<"enter value of a: ";
+    cin>>a;
+    cout<<"enter value of b: ";
+    cin>>b;
+    cout<<"enter value of c: ";
+    cin>>c;
+
+    dm = (b*b)-(4*a*c);
+    if(a!=0){
+        if(dm>0){
+            x1= (-b + (sqrt(dm)))/(2*a);
+            x2= (-b - (sqrt(dm)))/(2*a);
+            cout<<"The first result is "<<x1<<endl;
+            cout<<"The second result is "<<x2<<endl;
+        }
+        else{
+            if(dm=0){
+                x1 = x2 = (-b)/(2*a);
+                cout<<"The first result is "<<x1<<endl;
+                cout<<"The second result is "<<x2<<endl;
+            }
+            else{
+                cout<<"The result is imaginary";
+            }
+        }
+    }
+    else{
+        cout<<"a can't be 0";
+    }
+}
+*/
