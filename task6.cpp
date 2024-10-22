42a99e821c0a40ef15988d24b3744bd3ab1c4319
nermeen03 <nermeen41292@gmail.com>
2024-10-23 00:18:42 +0300

    Add files via upload
    finding ASCII code for any key in the keyboard

---
+++ task6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+/*
+int main(){
+
+    char ch;
+    ch=getch();
+    if(ch==-32){
+        ch = getch();
+        cout<<(int)ch;
+    }
+    else{
+        cout<<(int)ch;
+    }
+
+
+
+
+}
+*/
+
