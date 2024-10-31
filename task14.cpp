9a2b1690e05ae566af33a6237720fd5d6e540e5a
nermeen03 <nermeen41292@gmail.com>
2024-10-31 22:18:35 +0300

    Swap (call by value) and (call by address)

---
+++ task14.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+/*
+using namespace std;
+
+void Swap(int a,int b);
+void Ref(int* a,int* b);
+
+int main()
+{
+int x,y;
+cout<<"enter 1st value: ";
+cin>>x;
+cout<<"enter 2nd value: ";
+cin>>y;
+Swap(x,y);
+cout<<"1st is: ";
+cout<<x<<endl;
+cout<<"2nd is: ";
+cout<<y;
+
+Ref(&x,&y);
+cout<<"1st is: ";
+cout<<x<<endl;
+cout<<"2nd is: ";
+cout<<y<<endl;
+
+    return 0;
+}
+
+void Swap(int a,int b){
+int temp;
+temp=a;
+a=b;
+b=temp;
+}
+
+void Ref(int* a,int* b){
+int temp= *a;
+*a=*b;
+*b=temp;
+}
+*/
